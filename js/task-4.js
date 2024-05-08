08702c7da881928b29127e6e7fb9a925d98eecb9
Kasiaklaq <kasiaczyzewskaa@gmail.com>
2024-05-08 18:07:44 +0200

    summary

---
+++ js/task-4.js
@@ -0,0 +1,20 @@
+function getShippingCost(country) {
+    let price;
+    switch (country.toLowerCase()) {
+        case "china":
+            price = 100;
+            break;
+        case "chile":
+            price = 250;
+            break;
+        case "australia":
+            price = 170;
+            break;
+        case "jamaica":
+            price = 120;
+            break;
+        default:
+            return "Sorry, there is no delivery to your country";
+    }
+    return `Shipping to ${country} will cost ${price} credits`;
+}
